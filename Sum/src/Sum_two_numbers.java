75d6dbd864310c587e4938dd25c8fc65e39ae20f
prashantk27 <rockingwrickindia@gmail.com>
2016-09-17 20:58:36 +0530

    Sum

---
+++ Sum/src/Sum_two_numbers.java
@@ -0,0 +1,13 @@
+
+public class Sum_two_numbers {
+
+	public static void main(String[] args) {
+		int number1 = 10;
+		float number2 = (float)15.55;
+		
+		int total = (int) (number1+number2);
+			System.out.println(total);
+
+	}
+
+}
